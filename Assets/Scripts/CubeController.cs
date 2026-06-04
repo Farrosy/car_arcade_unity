201d0833439228fcdc6f029417557ddfc910fd3b
Farrosy <faros1q2w@gmail.com>
2026-06-04 21:20:25 +0700

    fix: cube controller

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -3,15 +3,22 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class CubeController : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed;
+    [Header("Movement Settings")]
+    [SerializeField] private float _moveSpeed = 15f;
+    [SerializeField] private float _turnSpeed = 100f; // Kecepatan belok mobil
     [SerializeField] private float _scale;
-    [SerializeField] private float _rotationSensivity;
-    [SerializeField] private float _minRotationX = -45f;
-    [SerializeField] private float _maxRotationX = 45f;
+    
+    // Kita pisahkan sensitivitas mouse jika kamu ingin kamera tetap bisa menengadah/menunduk
+    [Header("Camera Look Settings")]
+    [SerializeField] private float _mouseSensivity = 2f;
+    [SerializeField] private float _minRotationX = -30f;
+    [SerializeField] private float _maxRotationX = 30f;
+    
     public string Brand;
     private Transform myTransform;
     private Rigidbody myRigidbody;
-    public float RotationY;
+    
+    // Input variables
     public float InputVertical, InputHorizontal, MouseY, MouseX;
 
     private float currentRotationX;
@@ -24,101 +31,103 @@ public class CubeController : MonoBehaviour
 
     private void Awake()
     {
-        // Debug.Log("Awake");
         Inisialisasi();
     }
+
     private void Inisialisasi()
     {
-        myTransform = GetComponent<Transform>(); //mencri component pada game object yang sama
+        myTransform = GetComponent<Transform>();
         myRigidbody = GetComponent<Rigidbody>();
 
+        // Mengoptimalkan Rigidbody untuk pergerakan halus
         myRigidbody.interpolation = RigidbodyInterpolation.Interpolate;
         myRigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
-        myRigidbody.constraints |= RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        
+        // Mengunci rotasi Fisika agar mobil tidak guling saat menabrak
+        myRigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
 
         currentRotationY = myTransform.eulerAngles.y;
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        // Debug.Log("Start");
-        // Debug.Log($"Mobil brand {Brand} memiliki speed {_moveSpeed} ");
         Scale();
     }
-    private void OnEnable()
-    {
-        // Debug.Log("OnEnable");
-    }
-    private void OnDisable()
-    {
-        // Debug.Log("OnDisable");
-    }
 
-
-    public void Move()
-    {
-        // Debug.Log($"Mobil brand {Brand} on start pada posisi {myTransform.position} ");
-        Vector3 movement = (myTransform.forward * InputVertical) + (myTransform.right * InputHorizontal);
-        movement = Vector3.ClampMagnitude(movement, 1f);
-
-        Vector3 targetVelocity = movement * _moveSpeed;
-        targetVelocity.y = myRigidbody.linearVelocity.y;
-        myRigidbody.linearVelocity = targetVelocity;
-
-        // Debug.Log($"Mobil brand {Brand} setelah bergerak ada pada posisi {myTransform.position} ");
-    }
-
-    public void Rotate()
-    {
-        // Debug.Log($"Mobil brand {Brand} on start pada rotasi {myTransform.localEulerAngles} ");
-
-        currentRotationY += MouseX* _rotationSensivity;
-        currentRotationX -= MouseY* _rotationSensivity;
-
-        currentRotationX = Mathf.Clamp(currentRotationX, _minRotationX, _maxRotationX);
-
-        if (MainCameraTransform != null)
-        {
-            MainCameraTransform.localRotation = Quaternion.Euler(currentRotationX, 0f, 0f);
-        }
-        // Debug.Log($"Mobil brand {Brand} setelah bergerak pada rotasi {myTransform.rotation} ");
-    }
-
-    public void Scale()
-    {
-        // Debug.Log($"Mobil brand {Brand} on start pada scale {myTransform.localScale} ");
-        myTransform.localScale += (Vector3.one * _scale);
-        // Debug.Log($"Mobil brand {Brand} setelah bergerak pada scale {myTransform.localScale} ");
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // Debug.Log("Update");
         GetInput();
-        Rotate();
-        // Scale();
-    }
-
-    public void GetInput()
-    {
-        InputVertical = Input.GetAxis("Vertical");
-        InputHorizontal = Input.GetAxis("Horizontal");
-
-        MouseX = Input.GetAxis("Mouse X");
-        MouseY = Input.GetAxis("Mouse Y");
+        
+        // Kalkulasi rotasi kamera (melihat atas/bawah) dilakukan di Update agar responsif
+        RotateCamera(); 
     }
 
     private void FixedUpdate()
     {
+        // Kontrol fisik dijalankan di FixedUpdate
         Move();
-        myRigidbody.MoveRotation(Quaternion.Euler(0f, currentRotationY, 0f));
+        Turn();
     }
-     private void OnDestroy()
-     {
-        // Debug.Log("OnDestroy");
-     }
-     public void AddPoint(int amount)
+
+    public void GetInput()
+    {
+        // W / S atau Arrow Up / Down
+        InputVertical = Input.GetAxis("Vertical"); 
+        // A / D atau Arrow Left / Right
+        InputHorizontal = Input.GetAxis("Horizontal"); 
+
+        // Input Mouse untuk menengadah/menunduk
+        MouseY = Input.GetAxis("Mouse Y");
+    }
+
+    public void Move()
+    {
+        // Mobil hanya bergerak maju atau mundur searah dengan body-nya sendiri (myTransform.forward)
+        Vector3 targetVelocity = myTransform.forward * InputVertical * _moveSpeed;
+        
+        // Tetap pertahankan efek gravitasi (kecepatan Y jatuh) yang sedang berjalan
+        targetVelocity.y = myRigidbody.linearVelocity.y;
+        
+        // Terapkan ke Rigidbody
+        myRigidbody.linearVelocity = targetVelocity;
+    }
+
+    public void Turn()
+    {
+        // Mobil hanya bisa berbelok KETIKA sedang bergerak (maju atau mundur)
+        // Jika kamu ingin mobil bisa berbelok saat diam, hapus bagian "if" ini
+        if (Mathf.Abs(InputVertical) > 0.01f)
+        {
+            // Jika mundur (InputVertical negatif), arah belok biasanya dibalik agar natural
+            float directionModifier = InputVertical > 0 ? 1f : -1f;
+
+            // Hitung perubahan sudut rotasi Y berdasarkan input A/D
+            float turnAmount = InputHorizontal * _turnSpeed * directionModifier * Time.fixedDeltaTime;
+            currentRotationY += turnAmount;
+
+            // Terapkan rotasi horizontal ke Rigidbody
+            myRigidbody.MoveRotation(Quaternion.Euler(0f, currentRotationY, 0f));
+        }
+    }
+
+    public void RotateCamera()
+    {
+        // Mengatur kamera melihat ke atas / bawah secara independen menggunakan mouse Y
+        if (MainCameraTransform != null)
+        {
+            currentRotationX -= MouseY * _mouseSensivity;
+            currentRotationX = Mathf.Clamp(currentRotationX, _minRotationX, _maxRotationX);
+            
+            MainCameraTransform.localRotation = Quaternion.Euler(currentRotationX, 0f, 0f);
+        }
+    }
+
+    public void Scale()
+    {
+        myTransform.localScale += (Vector3.one * _scale);
+    }
+
+    public void AddPoint(int amount)
     {
         TotalPoints += amount;
         Debug.Log("Poin Bertambah! Total Poin Sekarang: " + TotalPoints);
